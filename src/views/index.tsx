ecf1bbb1a383e7ee71c43dbbfac94abeb4a42675
周顺顺 <zhoushunshun@k12cloud.cn>
2020-12-24 17:24:46 +0800

    🐎ci(全局): 初步完成
    less使用  路由都已经完成  下一步进行state  事件等

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,14 +1,14 @@
-import { Component } from 'react';
-import {style} from './index.module.less'
-export default class Index extends Component{
+import React from 'react';
+import './index.less'
+export default class Index extends React.Component<any, any>{
   constructor(props: object){
     super(props)
     this.state = {
-      num:1
+      num: 1
     }
   }
 
   render(){
-    return <div className={style.index}>index</div>
+    return <div className="index">{this.state.num}</div>
   }
 }
